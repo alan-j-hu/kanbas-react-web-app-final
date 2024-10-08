b45490eb9654d5512a51907aa2cd6919253676fa
Alan Hu <alanh@ccs.neu.edu>
2024-10-08 19:21:57 -0400

    Commit lab3 work

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,25 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInLine from "./ConditionalOutputInLine";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+
 export default function Lab3() {
   return (
     <div>
       <h2>Lab 3</h2>
+      <VariablesAndConstants/>
+      <VariableTypes/>
+      <IfElse/>
+      <TernaryOperator/>
+      <ConditionalOutputIfElse/>
+      <ConditionalOutputInLine/>
+      <LegacyFunctions/>
+      <ArrowFunctions/>
     </div>
   );
 }
