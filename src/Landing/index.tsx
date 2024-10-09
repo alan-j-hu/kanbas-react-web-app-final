25d0c7ed14fbddf02d8c1f4d41f326cbdf63bbed
Alan Hu <alanh@ccs.neu.edu>
2024-10-08 21:06:40 -0400

    Add real landing page

---
+++ src/Landing/index.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function Landing() {
+  return (
+    <div className="container">
+      <h1>Landing</h1>
+      <ul>
+        <li>Name: Alan Hu</li>
+        <li>GitHub: https://github.com/alan-j-hu/kanbas-react-web-app</li>
+        <li>Section: Tuesday/Friday 3:25 PM</li>
+        <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><Link to="/Labs/Lab2/">Lab 2</Link></li>
+      </ul>
+    </div>
+  );
+}
