cfc20fbf4c93823b9bddf873aec2180f3bcbdf83
Alan Hu <alanh@ccs.neu.edu>
2024-10-18 15:29:55 -0400

    Commit last class's work

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -7,8 +7,31 @@ import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInLine from "./ConditionalOutputInLine";
 import LegacyFunctions from "./LegacyFunctions";
 import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports"
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import PathParameters from "./PathParameters";
 
 export default function Lab3() {
+  console.log('Hello World!');
   return (
     <div>
       <h2>Lab 3</h2>
@@ -20,6 +43,33 @@ export default function Lab3() {
       <ConditionalOutputInLine/>
       <LegacyFunctions/>
       <ArrowFunctions/>
+      <ImpliedReturn/>
+      <TemplateLiterals/>
+      <SimpleArrays/>
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FindIndex/>
+      <FilterFunction/>
+      <JsonStringify/>
+      <House/>
+      <TodoList/>
+      <Spreading/>
+      <Destructing/>
+      <DestructingImports/>
+      <Classes/>
+      <Styles/>
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
+      <PathParameters/>
     </div>
   );
 }
