4b8ae11c70b0ee487864376bbe93317ea62df97e
Alan Hu <alanh@ccs.neu.edu>
2024-10-21 09:36:55 -0400

    Commit work so far

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,15 +1,23 @@
+import { courses } from "../Database";
+import { FaAlignJustify } from "react-icons/fa6";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import PeopleTable from "./People/Table";
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route, Navigate, useParams } from "react-router";
 
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  console.log(cid);
   return (
     <div id="wd-courses">
-      <h2 className="text-danger">Course 1234</h2>
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        {course && course.name}
+      </h2>
       <hr />
       <div className="d-flex">
         <div className="d-none d-md-block">
--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,81 +1,30 @@
 import { Link } from "react-router-dom";
-
-function Course(props: any) {
-  return (
-    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-      <div className="card rounded-3 overflow-hidden">
-        <Link className="wd-dashboard-course-link
-                         text-decoration-none text-dark"
-              to="/Kanbas/Courses/1234/Home">
-          <img src={props.image}
-               width="100%" height={160}/>
-          <div className="card-body">
-            <h5 className="wd-dashboard-course-title card-title">
-              {props.title}
-            </h5>
-            <p className="wd-dashboard-course-title card-text">
-              {props.name}
-            </p>
-            <button className="btn btn-primary"> Go </button>
-          </div>
-        </Link>
-      </div>
-    </div>
-  );
-}
-
+import * as db from "./Database";
 export default function Dashboard() {
+  const courses = db.courses;
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses" className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link className="wd-dashboard-course-link
-                               text-decoration-none text-dark"
-                    to="/Kanbas/Courses/1234/Home">
-                <img src="/images/reactjs.jpg"
-                     width="100%" height={160}/>
-                <div className="card-body">
-                  <h5 className="wd-dashboard-course-title card-title">
-                     CS1234 React JS
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                    Full Stack software developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
+          {courses.map((course) => (
+            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+              <div className="card rounded-3 overflow-hidden">
+                <Link to={`/Kanbas/Courses/${course._id}/Home`}
+                      className="wd-dashboard-course-link text-decoration-none text-dark" >
+                  <img src="/images/reactjs.jpg" width="100%" height={160} />
+                  <div className="card-body">
+                    <h5 className="wd-dashboard-course-title card-title">
+                      {course.name} </h5>
+                    <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
+                      {course.description} </p>
+                    <button className="btn btn-primary"> Go </button>
+                  </div>
+                </Link>
+              </div>
             </div>
-          </div>
-          <Course
-            image="/images/Video_Game_Championships_logo.png"
-            name="POKE1996 Pokemon VGC"
-            title="Competitive Pokemon Battling"/>
-          <Course
-            image="/images/1200px-Racket-logo.svg_-1024x1024.png"
-            name="CS2500 Fundies"
-            title="Fundamentals of Computer Science"/>
-          <Course
-            image="/images/CuboRubik.png"
-            name="MATH3175 Group Theory"
-            title="Group Theory"/>
-          <Course
-            image="/images/heliana.jpg"
-            name="CAT1234 Cat Language"
-            title="Mrrp"/>
-          <Course
-            image="/images/Linear_RGB_color_wheel.png"
-            name="ART101 Intro to Art"
-            title="Introduction to Art and Composition"/>
-          <Course
-            image="/images/cpp_logo.png"
-            name="CS679 C++ Programming"
-            title="Intro to Modern C++"/>
+          ))}
         </div>
       </div>
-    </div>
-  );
-}
+    </div>);}
