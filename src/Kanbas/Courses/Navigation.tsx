e93ca89badadb9082fc2e0dc6a219406cc3f6ccf
Alan Hu <alanh@ccs.neu.edu>
2024-10-21 20:25:51 -0400

    Complete course navigation

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
 export default function CoursesNavigation() {
+  const { cid } = useParams();
   return (
     <div className="wd list-group fs-5 rounded-0">
-      <Link to="/Kanbas/Courses/1234/Home" className="list-group-item active border border-0">Home</Link><br/>
-      <Link to="/Kanbas/Courses/1234/Modules" className="list-group-item text-danger border border-0">Modules
+      <Link to={`/Kanbas/Courses/${cid}/Home`} className="list-group-item active border border-0">Home</Link><br/>
+      <Link to={`/Kanbas/Courses/${cid}/Modules`} className="list-group-item text-danger border border-0">Modules
         </Link><br/>
-      <Link to="/Kanbas/Courses/1234/Piazza" className="list-group-item text-danger border border-0">Piazza</Link><br/>
-      <Link to="/Kanbas/Courses/1234/Zoom" className="list-group-item text-danger border border-0">Zoom</Link><br/>
-      <Link to="/Kanbas/Courses/1234/Assignments" className="list-group-item text-danger border border-0">
+      <Link to={`/Kanbas/Courses/${cid}/Piazza`} className="list-group-item text-danger border border-0">Piazza</Link><br/>
+      <Link to={`/Kanbas/Courses/${cid}/Zoom`} className="list-group-item text-danger border border-0">Zoom</Link><br/>
+      <Link to={`/Kanbas/Courses/${cid}/Assignments`} className="list-group-item text-danger border border-0">
           Assignments</Link><br/>
-      <Link to="/Kanbas/Courses/1234/Quizzes" className="list-group-item text-danger border border-0">Quizzes</Link><br/>
-      <Link to="/Kanbas/Courses/1234/Grades" className="list-group-item text-danger border border-0">Grades</Link><br/>
-      <Link to="/Kanbas/Courses/1234/People" className="list-group-item text-danger border border-0">People</Link><br/>
+      <Link to={`/Kanbas/Courses/${cid}/Quizzes`} className="list-group-item text-danger border border-0">Quizzes</Link><br/>
+      <Link to={`/Kanbas/Courses/${cid}/Grades`} className="list-group-item text-danger border border-0">Grades</Link><br/>
+      <Link to={`/Kanbas/Courses/${cid}/People`} className="list-group-item text-danger border border-0">People</Link><br/>
     </div>
 );}
