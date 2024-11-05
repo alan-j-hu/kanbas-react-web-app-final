b24d260e624e59654a41b469214e07e78c4693d0
Alan Hu <alanh@ccs.neu.edu>
2024-11-05 09:35:14 -0500

    Add enroll/unenroll UI

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -13,54 +13,72 @@ export default function Dashboard(
   const fallback = "reactjs.jpg";
 
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const { enrollments } = db;
+  const enrollments = db.enrollments.filter(enrollment => enrollment.user = currentUser._id);
 
-  const newCourse = currentUser.role === "FACULTY"
-    ? <>
+  const [allCourses, setAllCourses] = useState(false);
+
+  function NewCourse() {
+    return <>
       <h5>New Course
-          <button className="btn btn-primary float-end"
-                  id="wd-add-new-course-click"
-                  onClick={addNewCourse} > Add </button>
+        <button className="btn btn-primary float-end"
+                id="wd-add-new-course-click"
+                onClick={addNewCourse} > Add </button>
       </h5><br />
       <input defaultValue={course.name} className="form-control mb-2"
              onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
       <textarea defaultValue={course.description} className="form-control"
-             onChange={(e) => setCourse({ ...course, description: e.target.value }) } />
+                onChange={(e) => setCourse({ ...course, description: e.target.value }) } />
       <hr />
-    </>
-    : <></>;
+    </>;
+  }
 
-  const deleteUpdate = currentUser.role === "FACULTY"
-    ? <>
-        <button onClick={(event) => {
-                  event.preventDefault();
-                  deleteCourse(course._id);
-                }} className="btn btn-danger float-end"
-                id="wd-delete-course-click">
-          Delete
-        </button>
-        <button className="btn btn-warning float-end me-2"
-                onClick={updateCourse} id="wd-update-course-click">
-          Update
-        </button>
-      </>
-    : <></>;
+  function ToggleAllCourses() {
+    return <>
+      <button aria-pressed={allCourses}
+              className={`btn btn-primary ${allCourses ? "active" : ""} float-end`}
+              onClick={() => setAllCourses(!allCourses)}>
+        Show All Courses
+      </button>
+    </>;
+  }
+
+  function DeleteUpdate() {
+    return <>
+      <button onClick={(event) => {
+                event.preventDefault();
+                deleteCourse(course._id);
+              }}
+              className="btn btn-danger float-end"
+              id="wd-delete-course-click">
+        Delete
+      </button>
+      <button className="btn btn-warning float-end me-2"
+              onClick={updateCourse} id="wd-update-course-click">
+        Update
+      </button>
+    </>;
+  }
+
+  function EnrollUnenroll({ course } : { course : any }) {
+    const enrolled = enrollments.some((enrollment) => enrollment.course === course._id);
+    return <button className={`btn ${enrolled ? "btn-danger" : "btn-success"} float-end`}>
+      {enrolled ? "Unenroll" : "Enroll"}
+    </button>
+  }
 
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      {newCourse}
+      {currentUser.role === "FACULTY" ? <NewCourse/> :
+       currentUser.role === "STUDENT" ? <ToggleAllCourses/> :
+       <></>}
 
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses" className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses
             .filter((course) =>
-              enrollments.some(
-                (enrollment) =>
-                  enrollment.user === currentUser._id &&
-                  enrollment.course === course._id
-                 ))
+              allCourses || enrollments.some((enrollment) => enrollment.course === course._id))
             .map((course) => (
               <div className="wd-dashboard-course col" style={{ width: "300px" }}>
                 <div className="card rounded-3 overflow-hidden">
@@ -73,7 +91,9 @@ export default function Dashboard(
                       <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
                         {course.description} </p>
                       <button className="btn btn-primary"> Go </button>
-                      {deleteUpdate}
+                      {currentUser.role === "FACULTY" ? <DeleteUpdate/> :
+                       currentUser.role === "STUDENT" ? <EnrollUnenroll course={course}/> :
+                       <></>}
                     </div>
                   </Link>
                 </div>
