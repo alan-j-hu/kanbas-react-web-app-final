ac810e6d2f6dee97ff2e7d41a6ffc98d4d2eab02
Alan Hu <alanh@ccs.neu.edu>
2024-11-05 05:46:48 -0500

    Implement assignment editor screen

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,19 +1,33 @@
-import { assignments } from "../../Database";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Routes, Route, Navigate, useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import { updateAssignment } from "./reducer";
 
 export default function AssignmentEditor() {
   const { aid } = useParams();
+  const { assignments } = useSelector((state: any) => state.assignmentsReducer);
   const assignment = assignments.find((assignment: any) => assignment._id === aid);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState(assignment && assignment.title);
+  const [start, setStart] = useState(assignment && assignment.start);
+  const [due, setDue] = useState(assignment && assignment.due);
+  const [until, setUntil] = useState(assignment && assignment.until);
+  const [points, setPoints] = useState(assignment && assignment.points);
+  const [description, setDescription] = useState(assignment && assignment.description);
+
   return (
     <div id="wd-assignments-editor">
       <div className="mb-3">
         <label htmlFor="wd-name">Assignment Name</label>
-        <input id="wd-name" className="form-control" value={assignment && assignment.title} />
+        <input id="wd-name" className="form-control" value={title}
+               onChange={e => setTitle(e.target.value)}/>
       </div>
       <div className="mb-3">
-        <textarea id="wd-description" className="form-control">
-          {assignment && assignment.description}
-        </textarea>
+        <textarea id="wd-description" className="form-control" value={description}
+          onChange={(e) => setDescription(e.target.value)}/>
       </div>
       <br />
       <div>
@@ -22,7 +36,10 @@ export default function AssignmentEditor() {
             <label htmlFor="wd-points">Points</label>
           </div>
           <div className="col-sm-2">
-            <input id="wd-points" className="form-control" value={assignment && assignment.points} />
+            <input id="wd-points" className="form-control" value={points}
+                   onChange={(e) => {
+                     setPoints(parseInt(e.target.value));
+                   }}/>
           </div>
         </div>
         <div className="row mb-3">
@@ -83,24 +100,46 @@ export default function AssignmentEditor() {
             <label htmlFor="wd-due-date">Due</label><br />
             <input className="form-control" type="date"
               id="wd-due-date"
-              value={assignment && assignment.due}/><br/>
+              value={due}
+              onChange={e => setDue(e.target.value)}/><br/>
 
             <div className="row">
               <div className="col">
                 <label htmlFor="wd-available-from">Available from</label><br />
                 <input type="date" className="form-control"
                   id="wd-available-from"
-                  value={assignment && assignment.start}/><br/>
+                  value={start}
+                  onChange={e => setStart(e.target.value)}/><br/>
               </div>
               <div className="col">
                 <label htmlFor="wd-available-until">Until</label><br />
                 <input type="date" className="form-control"
-                  id="wd-available-until"/><br/>
+                  id="wd-available-until"
+                  value={until}
+                  onChange={e => setUntil(e.target.value)}/><br/>
               </div>
             </div>
           </div>
         </div>
-        {/* Complete on your own */}
+        <div className="modal-footer">
+          <Link className="btn btn-secondary" to={`/Kanbas/Courses/${assignment.course}/Assignments`}>
+            Cancel
+          </Link>
+          <button onClick={() => {
+              dispatch(updateAssignment({
+                ...assignment,
+                title,
+                start,
+                due,
+                until,
+                points,
+                description,
+              }));
+              navigate(`/Kanbas/Courses/${assignment.course}/Assignments`);
+            }}
+            type="button" className="btn btn-danger">
+            Add Assignment </button>
+        </div>
       </div>
     </div>
 );}
