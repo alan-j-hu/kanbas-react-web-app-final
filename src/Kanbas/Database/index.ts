74c77e808763ac1c9f9a434e060f906d32cce321
Alan Hu <alanh@ccs.neu.edu>
2024-11-15 17:14:25 -0500

    Commit classwork

--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -1,8 +1,6 @@
-import courses from "./courses.json";
-import modules from "./modules.json";
-import assignments from "./assignments.json";
-import enrollments from "./enrollments.json";
-import users from "./users.json";
-
-export {
-  courses, modules, assignments, enrollments, users };
+import courses from "./courses.js";
+import modules from "./modules.js";
+import assignments from "./assignments.js";
+import users from "./users.js";
+import enrollments from "./enrollments.js";
+export { courses, modules, assignments, users, enrollments };
