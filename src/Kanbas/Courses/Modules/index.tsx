64af5cbd3f6aaeac0df41f5d837feb96b5aa5eca
Alan Hu <alanh@ccs.neu.edu>
2024-11-22 05:27:14 -0500

    Progress

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -4,17 +4,32 @@ import LessonControlButtons from "./LessonControlButtons";
 import ModuleControlButtons from "./ModuleControlButtons";
 import ModulesControls from "./ModulesControls";
 import { useParams } from "react-router";
-import * as db from "../../Database";
 import { setModules, addModule, editModule, updateModule, deleteModule }
   from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
 import * as coursesClient from "../client";
+import * as modulesClient from "./client";
 
 export default function Modules() {
   const { cid } = useParams();
   const [moduleName, setModuleName] = useState("");
   const { modules } = useSelector((state: any) => state.modulesReducer);
   const dispatch = useDispatch();
+  const saveModule = async (module: any) => {
+    await modulesClient.updateModule(module);
+    dispatch(updateModule(module));
+  };
+  const removeModule = async (moduleId: string) => {
+    await modulesClient.deleteModule(moduleId);
+    dispatch(deleteModule(moduleId));
+  };
+  const createModuleForCourse = async () => {
+    if (!cid) return;
+    const newModule = { name: moduleName, course: cid };
+    const module = await coursesClient.createModuleForCourse(cid, newModule);
+    dispatch(addModule(module));
+  };
+
   const fetchModules = async () => {
     const modules = await coursesClient.findModulesForCourse(cid as string);
     dispatch(setModules(modules));
@@ -27,10 +42,7 @@ export default function Modules() {
   return (
     <div>
       <ModulesControls moduleName={moduleName} setModuleName={setModuleName}
-        addModule={() => {
-          dispatch(addModule({ name: moduleName, course: cid }));
-          setModuleName("");
-        }} />
+        addModule={createModuleForCourse}/>
       <br /><br /><br /><br />
       <ul id="wd-modules" className="list-group rounded-0">
         {modules
@@ -46,15 +58,13 @@ export default function Modules() {
                onChange={(e) => dispatch(updateModule({ ...module, name: e.target.value }))}
                onKeyDown={(e) => {
                  if (e.key === "Enter") {
-                   dispatch(updateModule({ ...module, editing: false }));
+                     saveModule({ ...module, editing: false });
                  }
                }}
                defaultValue={module.name}/>
       )}
       <ModuleControlButtons moduleId={module._id}
-        deleteModule={(moduleId) => {
-          dispatch(deleteModule(moduleId));
-        }}
+        deleteModule={(moduleId) => removeModule(moduleId)}
         editModule={(moduleId) => dispatch(editModule(moduleId))} />
 
 
--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -6,7 +6,17 @@ export const fetchAllCourses = async () => {
   return data;
 };
 
+export const createModuleForCourse = async (courseId: string, module: any) => {
+  const response = await axios.post(
+    `${COURSES_API}/${courseId}/modules`,
+    module
+  );
+  return response.data;
+};
+
 export const findModulesForCourse = async (courseId: string) => {
-  const { data } = await axios.get(`${COURSES_API}/${courseId}/modules`);
-  return data;
-}
+  const response = await axios
+    .get(`${COURSES_API}/${courseId}/modules`);
+  return response.data;
+};
+
