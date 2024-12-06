2bfde91869ce10b07bca4a6b3fbffd76373876c2
Alan Hu <alanh@ccs.neu.edu>
2024-12-06 07:28:16 -0500

    Document login info

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -8,6 +8,7 @@ export default function Landing() {
         <li>Name: Alan Hu</li>
         <li>Client GitHub: https://github.com/alan-j-hu/kanbas-react-web-app</li>
         <li>Server GitHub: https://github.com/alan-j-hu/kanbas-node-server-app</li>
+        <li>Login info: https://github.com/alan-j-hu/kanbas-node-server-app/blob/a6/data/kanbas.users.json</li>
         <li>Section: Tuesday/Friday 3:25 PM</li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
         <li><Link to="/Labs/Lab5/">Lab 5</Link></li>
